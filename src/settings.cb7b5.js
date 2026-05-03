faf1bd33ce6f232e7e58056350c5a92fbf508011
Ksandr Nelar <ksandr.nelar@gmail.com>
2026-05-03 22:30:42 +0400

    add build

---
+++ src/settings.cb7b5.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/game.fire",orientation:"portrait",jsList:[],bundleVers:{resources:"7593f",internal:"d17e2",main:"eb123"}};
